cef5cb7b59b018bf5e10db99dbe9a287024f2439
Coder Najim <najim@gmail.com>
2024-11-22 11:07:50 +0530

    fetch all data from and display on the page

---
+++ script.js
@@ -0,0 +1,23 @@
+const contriesContainer = document.querySelector(".countries-container");
+fetch("https://restcountries.com/v3.1/all ")
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((countries) => {
+      console.log(countries);
+      const containerCard = document.createElement("a");
+      containerCard.classList.add("countries-content");
+      containerCard.href = `/coutry.html?name=${countries.name.common}`;
+
+      containerCard.innerHTML = `
+         
+         <img src="${countries.flags.svg}" alt="">
+            <div class="countries-text">
+                <h3 class="countries-title">${countries.name.common}</h3>
+                <p><b>Population: </b>${countries.population}</p>
+                <p><b>Region: </b>${countries.region}</p>
+                <p><b>Capital: </b>${countries.capital}</p>
+            
+            </div> `;
+      contriesContainer.append(containerCard);
+    });
+  });
