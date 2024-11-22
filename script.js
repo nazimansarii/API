9f8561bb0c6e44cfe6098ac50e4af7df4207082f
Coder Najim <najim@gmail.com>
2024-11-22 11:13:17 +0530

    fix spelling and formate population number

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ fetch("https://restcountries.com/v3.1/all ")
       console.log(countries);
       const containerCard = document.createElement("a");
       containerCard.classList.add("countries-content");
-      containerCard.href = `/coutry.html?name=${countries.name.common}`;
+      containerCard.href = `/country.html?name=${countries.name.common}`;
 
       containerCard.innerHTML = `
          
          <img src="${countries.flags.svg}" alt="">
             <div class="countries-text">
                 <h3 class="countries-title">${countries.name.common}</h3>
-                <p><b>Population: </b>${countries.population}</p>
+                <p><b>Population: </b>${countries.population.toLocaleString('en-IN')}</p>
                 <p><b>Region: </b>${countries.region}</p>
                 <p><b>Capital: </b>${countries.capital}</p>
             
